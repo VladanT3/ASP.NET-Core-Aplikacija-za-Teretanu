45cd40d30ba4855b90ee63484155de9e4dbb16fb
Vladan Tešić <74865851+VladanT3@users.noreply.github.com>
2023-05-20 19:01:04 +0200

    Add files via upload

---
+++ wwwroot/js/kontrolaKolicine.js
@@ -0,0 +1,11 @@
+﻿function povecajKolicinu(id) {
+    const kolicina = document.getElementById(id)
+    kolicina.value = parseInt(kolicina.value) + 1
+}
+function smanjiKolicinu(id) {
+    const kolicina = document.getElementById(id)
+    let kol = parseInt(kolicina.value)
+    if (kol == 1)
+        return
+    kolicina.value = kol - 1
+}
---
+++ wwwroot/js/prikazSifre.js
@@ -0,0 +1,9 @@
+﻿const prikazSifre = document.getElementById('prikazSifre');
+const sifra = document.getElementById('inputSifra');
+
+prikazSifre.addEventListener("change", function () {
+    if (prikazSifre.checked)
+        sifra.type = 'text';
+    else
+        sifra.type = 'password';
+});
